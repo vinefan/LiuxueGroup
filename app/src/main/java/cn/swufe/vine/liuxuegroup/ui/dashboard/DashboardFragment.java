ac8219f7c7c51b8aa1505dbae594c1ffb369beaa
vinefan <17360078826@163.com>
2020-11-03 20:34:51 +0800

    test fragment

--- app/src/main/java/cn/swufe/vine/liuxuegroup/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/cn/swufe/vine/liuxuegroup/ui/dashboard/DashboardFragment.java
@@ -25,12 +25,15 @@ public class DashboardFragment extends Fragment {
 //        将fragment填充进activity
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
+//        取出文本框视图
         final TextView textView = root.findViewById(R.id.text_dashboard);
 
+//        执行dashboardViewModel的getText方法，
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+//
+//                textView.setText(s);
             }
         });
 
